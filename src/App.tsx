5535b3dbdc6199e0c0691ba1acaf5e5e6b85d792
mash0809 <tonem0809@gmail.com>
2026-06-27 22:23:33 +0900

    feat: 라우터와 보호 라우트 구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,26 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import ProtectedRoute from './components/common/ProtectedRoute'
+import LoginPage from './pages/LoginPage'
+import MapPage from './pages/MapPage'
+import MyPage from './pages/MyPage'
+import SignupPage from './pages/SignupPage'
+
+const queryClient = new QueryClient()
+
 export default function App() {
-  return <div>boombeene-fe</div>
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route element={<ProtectedRoute />}>
+            <Route path="/" element={<MapPage />} />
+            <Route path="/my" element={<MyPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  )
 }
